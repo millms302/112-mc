928a06e317986c7c65623c466dc0c1e1465422b9
Mike Miller <millms302@gmail.com>
2026-01-15 21:54:22 -0600

    end of class 4

--- posts/urls.py
+++ posts/urls.py
@@ -3,11 +3,15 @@ from .views import (
     PostListView,
     PostDetailView,
     PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
 )
 
 urlpatterns=[
     path("",PostListView.as_view(), name="post_list"),
     path("<int:pk>",PostDetailView.as_view(), name="post_detail"),
-    path("new/",PostCreateView.as_view(), name="post_new")
+    path("new/",PostCreateView.as_view(), name="post_new"),
+    path("edit/<int:pk>",PostUpdateView.as_view(), name="post_edit"),
+    path("delete/<int:pk>/", PostDeleteView.as_view(), name="post_delete")
 
 ]
--- posts/views.py
+++ posts/views.py
@@ -6,6 +6,7 @@ from django.views.generic import (
     DeleteView,
 )
 from .models import Post
+from django.urls import reverse_lazy
 
 class PostListView(ListView):
     """
@@ -36,4 +37,21 @@ class PostCreateView(CreateView):
     model = Post
     # Fields attribute allows us to control which elements to display on the form to create a new object
     # The name of the fields have to match with the attributes of the model.
-    fielse = ["title", "subtitle", "body"] 
+    fields = ["title", "subtitle", "body"] 
+
+class PostUpdateView(UpdateView):
+    """
+    """
+
+    template_name = "posts/edit.html"
+    model = Post
+    fields = ["title", "subtitle", "body"]
+
+class PostDeleteView(DeleteView):
+    """
+    Docstring for PostDeleteView
+    """
+
+    template_name = "posts/delete.html"
+    model = Post
+    success_url = reverse_lazy("post_list")
